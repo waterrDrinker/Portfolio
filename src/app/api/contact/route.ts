d4cc406a3edccc68daaaf1f4013d5d53287556ef
Grigory.N <grigoriynbr@gmail.com>
2026-04-14 06:56:47 +0300

    refactor: change contact from resend to telegram

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,31 +1,44 @@
-import { Resend } from 'resend';
-
-import EmailTemplate from '@/app/[lang]/(pages)/contact/components/EmailTempate';
-import { ContactFormData } from '@/app/[lang]/(pages)/contact/components/Form';
-import { EMAIL } from '@/shared/constants';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
-  const formData = await req.json();
-  const { subject } = formData as ContactFormData;
-
   try {
-    const { data, error } = await resend.emails.send({
-      from: 'Portfolio <onboarding@resend.dev>',
-      react: EmailTemplate({ formData }),
-      subject: subject || 'New contact message',
-      to: EMAIL,
-    });
+    const formData = await req.json();
+    const { email, message, name, subject } = formData;
 
-    if (error) {
-      console.error(error);
-      return Response.json({ error }, { status: 500 });
+    const text = `
+📩 New Portfolio Message
+
+👤 Name: ${name}
+📧 Email: ${email}
+📝 Subject: ${subject}
+
+💬 Message:
+${message}
+    `;
+
+    const res = await fetch(
+      `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        body: JSON.stringify({
+          chat_id: process.env.TELEGRAM_CHAT_ID,
+          parse_mode: 'HTML',
+          text,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      },
+    );
+
+    const data = await res.json();
+
+    if (!data.ok) {
+      console.error(data);
+      return Response.json({ error: data.description }, { status: 500 });
     }
 
-    return Response.json(data);
+    return Response.json({ success: true });
   } catch (error) {
     console.error(error);
-    return Response.json({ error }, { status: 500 });
+    return Response.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
